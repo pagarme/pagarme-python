9366754402fcdf10b6488d7737121609ae82b7cf
Nixsm <nichsmm@gmail.com>
2017-10-20 16:20:14 -0200

    Add test for the card hasher method using a random public key

--- tests/card_test.py
+++ tests/card_test.py
@@ -1,5 +1,5 @@
 from pagarme import card
-from tests.resources.dictionaries import card_dictionary
+from tests.resources.dictionaries import card_dictionary, public_key_dictionary
 
 
 def test_create_card():
@@ -17,3 +17,13 @@ def test_find_by():
     search_params = {'id': _card['id']}
     find_card = card.find_by(search_params)
     assert _card['id'] == find_card[0]['id']
+
+def test_hash():
+    _card = card.create(card_dictionary.VALID_CARD)
+    public_key = public_key_dictionary.PUBLIC_KEY
+    request_id = 402210
+    card_hash = card.hash(request_id, public_key, _card)
+
+    assert str(request_id) in card_hash
+    assert '==' in card_hash
+    assert '_' in card_hash
---
+++ tests/resources/dictionaries/public_key_dictionary.py
@@ -0,0 +1,10 @@
+PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----\n' \
+        'MIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEA7A0E1qiwWKG6f1IXVUUB\n' \
+        'ljWtEVyQ5BJ6Dx4L/r967ENeLadDdv96XgTe7CywOCZ5Yeu+KaDx7JbtT4XBZ1O0\n' \
+        'm0oard7bzXGmCbCafHA3FF832273OpCE4X7bcOLStWx9yaASgaxJYeWFEluHFa9V\n' \
+        'kOaP2UcqFCqzOF5BxtH5DH/40JsPdDJUc45S/TnzNasgdX65qSTGJeczmwPS0LaB\n' \
+        'NyaoN9ltEkWwe661aljqumWst1MPf3iBshKRW78vh392g+GyPt08q7bG7zGy5xba\n' \
+        'lhvkGz6laK4HKjKnURtiHbm0qoQR07aEckaUvzcQMOyoGAhdywK6h95g0sDCbUlg\n' \
+        'NQIBAw==' \
+        '\n-----END PUBLIC KEY-----\n'
+
