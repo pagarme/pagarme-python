f1499f70d76d4cc961a4ee6ce6259175caf10be3
alessandra1408 <alessandrafernandes.contato@gmail.com>
2024-04-29 15:48:39 -0300

    feat: adds register_information in recipient

--- tests/resources/dictionaries/recipient_dictionary.py
+++ tests/resources/dictionaries/recipient_dictionary.py
@@ -16,3 +16,32 @@ UPDATE_RECIPIENT = {
     'transfer_enabled': 'false',
     'anticipatable_volume_percentage': '85'
 }
+
+REGISTER_INFORMATION = {
+    'type': 'individual',
+    'document_number': '12345678910',
+    'name': 'Someone',
+    'site_url': '',
+    'email': 'some',
+    'mother_name': 'Eliana',
+    'birthdate': '01',
+    'monthly_income': '1000',
+    'professional_occupation': 'Empresário',
+    'address': {
+        'street': None,
+        'complementary': 'SN',
+        'street_number': None,
+        'neighborhood': None,
+        'city': None,
+        'state': None,
+        'zipcode': None,
+        'reference_point': None
+  },
+  'phone_numbers': [
+    {
+      'ddd': '27',
+      'number': '999992628',
+      'type': 'primary'
+    }
+  ]
+}
